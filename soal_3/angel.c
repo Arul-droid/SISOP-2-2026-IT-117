dbaf04e58fa4740967540a9276ac14ff4d4521a4
Arul-droid <5027251117@student.its.ac.id>
2026-04-08 16:11:25 +0700

    add semua file praktikum

--- soal_3/angel.c
+++ soal_3/angel.c
@@ -272,3 +272,6 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+//source
+//https://claude.ai/share/e80361d5-1379-4dec-9ff3-ba79a5d5c1f1 and arul's own code for soal_2/contract_daemon.c
